5e0db901a65890904f2f905cfdac34b767b9f029
Prashant-KT <tiwariprashant0395@gmail.com>
2022-07-15 11:46:38 +0530

    signIn UI done , data need to match , header position shld change on each page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       {/* <Navbar /> */}
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
--- src/Pages/LoginPage/Signin.jsx
+++ src/Pages/LoginPage/Signin.jsx
@@ -1,14 +1,80 @@
-import React from 'react'
-import style from './Signin.module.css'
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Navbar } from "../Navbar/Navbar";
+import style from "./Signin.module.css";
 export const Signin = () => {
+  const[number, setNumber] = useState("")
+  const [password, setPassword] = useState("");
+
+  const handleNumber = (e) =>{
+    alert(e.target.value)
+  }
+   const handlePassword = (e) => {
+     alert(e.target.value);
+   };
+
+   const handlelogin = () =>{
+    alert("hi")
+   }
   return (
-    <div className={style.signinCont}>
-      <div className={style.bigBox}>
-          <div>
-            <img src="https://www.netmeds.com/images/cms/wysiwyg/cms/1588773798_sign-in-banner-new.png" alt="" />
+    <div>
+      <div style={{ position: "static" }}>
+        <Navbar />
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className={style.signinCont}>
+        <div className={style.bigBox}>
+          <div className={style.bigBoxImage}>
+            <img
+              src="https://www.netmeds.com/images/cms/wysiwyg/cms/1588773798_sign-in-banner-new.png"
+              alt=""
+            />
           </div>
-          <div></div>
+          <div className={style.bigBoxLogin}>
+            <div>
+              <h2>Sign In / Sign Up</h2>
+              <p>
+                Sign up or Sign in to access your orders, special offers, health
+                tips and more!
+              </p>
+              {/* login email */}
+              <TextField
+                helperText="Please enter registered Mobile Number"
+                id="demo-helper-text-misaligned-no-helper"
+                label="Mobile Number"
+                style={{ width: "100%" }}
+                onChange={handleNumber}
+              />
+              <br></br>
+              <br></br>
+              {/* password */}
+              <TextField
+                helperText="Please enter Password"
+                id="demo-helper-text-misaligned-no-helper"
+                label="Password"
+                style={{ width: "100%" }}
+                onChange={handlePassword}
+              />
+              {/* submit */}
+              <br></br>
+              <br></br>
+              <Button
+                className={style.submitButton}
+                variant="contained"
+                color="success"
+                onClick = {handlelogin}
+              >
+                Submit
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
